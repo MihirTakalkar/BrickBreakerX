619b1537ae9cd1662d46269bbc62c6700a4bb419
Mihir Takalkar <papyarocks@gmail.com>
2017-04-16 12:34:46 -0700

    added paddles to game

---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MihirBrickBreaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics gfx;
+        Rectanglez paddle;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gfx = CreateGraphics();
+            paddle = new Rectanglez(228, ClientSize.Height - 15, 20, 150, 5);
+        }
+
+        //Location
+        public void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = string.Format(" Location: X:{0} Y:{1}", e.X, e.Y);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //erase
+            gfx.DrawImage(BackgroundImage, 0, 0, ClientSize.Width, ClientSize.Height);
+
+            paddle.Update();
+
+            //Paddle
+            paddle.Draw(gfx);
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right)
+            {
+                paddle.Right = true; 
+            }
+            
+            if(e.KeyCode == Keys.Left)
+            {
+                paddle.Left = true;
+            }
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                paddle.Left = false;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                paddle.Right = false;
+            }
+        }
+
+      
+    }
+}
---
+++ Rectanglez.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MihirBrickBreaker
+{
+    class Rectanglez
+    {
+        public int x;
+        public int y;
+        public int height;
+        public int width;
+        public int speedx;
+        Rectangle hitbox;
+
+        public bool Left = false;
+        public bool Right = false;
+
+        public Rectanglez(int x, int y, int height, int width, int speedx)
+        {
+            this.x = x;
+            this.y = y;
+            this.height = height;
+            this.width = width;
+            this.speedx = speedx;
+
+            hitbox = new Rectangle(x, y, width, height);
+        }
+
+        public void Update()
+        {
+            if (Left == true)
+            {
+                MoveLeft();
+            }
+        }
+
+
+        public void MoveLeft()
+        {
+            if (x > 0)
+            {
+                x -= speedx;
+            }
+        }
+
+        public void MoveRight(Size clientSize) //pass in the width of the screen
+        {
+            if (x > clientSize.Width - width)
+            {
+                x += speedx;
+            }
+        }
+
+        public void Draw(Graphics gfx)
+        {
+            gfx.FillRectangle(Brushes.Green, x, y, width, height);
+        }
+
+
+    }
+}
